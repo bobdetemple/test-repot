31ab77bcb7545140b93c0358b9fe316ba8b6801e
unknown <robertr@mlv-is-02.CHCI.com>
2014-07-10 16:16:03 -0400

    fixed merge issue, and added second comment

--- app.js
+++ app.js
@@ -2,7 +2,5 @@ var strings = require('./strings');
 //here is a comment
 console.log(strings.hello);
 console.log(strings.goodbye);
-<<<<<<< HEAD
 //here is a comment
-=======
->>>>>>> 5e86b7ea5b0e3f12fb385f83e54758482d6b3f2a
+
